58d311ff9fe7577f232550d76ae7dd2dc41b1207
Ryan Brewer <ryanbrew317@gmail.com>
2023-12-09 23:05:36 -0800

    Improve post-writing flow with a drafts folder and new_post.sh

---
+++ new_post.sh
@@ -0,0 +1,29 @@
+echo "Enter name:"
+read name
+echo "Enter id:"
+read id
+draft_filename="/Users/ryanbrewer/Documents/ryanbrewer-dev/drafts/$id.txt"
+prod_filename="/Users/ryanbrewer/Documents/ryanbrewer-dev/posts/$id.txt"
+if test -f $draft_filename;
+then
+  echo "That id is in use already!"
+else
+  if test -f $prod_filename;
+  then
+    echo "That id is in use already!"
+  else
+    IFS='' read -r -d '' String <<EOF
+id: $id
+name: $name
+date: $(date +'%Y-%m-%d')
+tags: 
+description: 
+
+@paragraph@
+Hello world!
+@end@
+EOF
+    touch $draft_filename
+    echo "${String}" >> $draft_filename
+  fi
+fi
