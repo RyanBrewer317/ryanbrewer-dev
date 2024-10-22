07bb556fea37c14a267cdc390dc0a768e4b3cbc7
Ryan Brewer <ryanbrew317@gmail.com>
2024-10-21 18:34:11 -0700

    fix routing hopefully

--- dist/priv/ryanbrewerdev/pages/unknown_page.mjs
+++ dist/priv/ryanbrewerdev/pages/unknown_page.mjs
@@ -36,6 +36,10 @@ export function unknown_page() {
               ),
             ]),
           ),
+          $html.script(
+            toList([]),
+            "\nif (window.location.pathname === '/search.html')\n  window.location.href = '/posts';\nif (window.location.pathname === '/search')\n  window.location.href = '/posts';\nif (window.location.pathname === '/contact.html')\n  window.location.href = '/contact';\nif (window.location.pathname === '/demos.html')\n  window.location.href = '/demos';\nif (window.location.pathname === '/404.html')\n  window.location.href = '/404';\nif (window.location.pathname === '/posts/announcing-svm.html')\n  window.location.href = '/posts/announcing-sabervm';\nif (window.location.pathname === '/posts/beginners-guide-pl-academia.html')\n  window.location.href = '/posts/beginners-guide-pl-academia';\nif (window.location.pathname === '/posts/first-post.html')\n  window.location.href = '/posts/first-post';\nif (window.location.pathname === '/posts/getting-started-category-theory.html')\n  window.location.href = '/posts/getting-started-category-theory';\nif (window.location.pathname === '/posts/implicit-products-better-forall.html')\n  window.location.href = '/posts/implicit-products-better-forall';\nif (window.location.pathname === '/posts/logic-in-types.html')\n  window.location.href = '/posts/logic-in-types';\nif (window.location.pathname === '/posts/safe-mmm-with-coeffects.html')\n  window.location.href = '/posts/safe-mmm-with-coeffects';\nif (window.location.pathname === '/posts/security-crashing-modal-logic.html')\n  window.location.href = '/posts/security-crashing-modal-logic';\nif (window.location.pathname === '/posts/simple-programming-languages.html')\n  window.location.href = '/posts/simple-programming-languages';\nif (window.location.pathname === '/posts/type-of-sprintf.html')\n  window.location.href = '/posts/type-of-sprintf';\nif (window.location.pathname === '/posts/typechecking-sabervm.html')\n  window.location.href = '/posts/typechecking-sabervm';\nif (window.location.pathname === '/posts/cricket.html')\n  window.location.href = '/posts/cricket';\n      ",
+          ),
           tail(),
         ]),
       ),
--- src/pages/unknown_page.gleam
+++ src/pages/unknown_page.gleam
@@ -21,6 +21,45 @@ pub fn unknown_page() -> Element(Nil) {
           text("Unknown Page (404)"),
         ]),
       ]),
+      html.script(
+        [],
+        "
+if (window.location.pathname === '/search.html')
+  window.location.href = '/posts';
+if (window.location.pathname === '/search')
+  window.location.href = '/posts';
+if (window.location.pathname === '/contact.html')
+  window.location.href = '/contact';
+if (window.location.pathname === '/demos.html')
+  window.location.href = '/demos';
+if (window.location.pathname === '/404.html')
+  window.location.href = '/404';
+if (window.location.pathname === '/posts/announcing-svm.html')
+  window.location.href = '/posts/announcing-sabervm';
+if (window.location.pathname === '/posts/beginners-guide-pl-academia.html')
+  window.location.href = '/posts/beginners-guide-pl-academia';
+if (window.location.pathname === '/posts/first-post.html')
+  window.location.href = '/posts/first-post';
+if (window.location.pathname === '/posts/getting-started-category-theory.html')
+  window.location.href = '/posts/getting-started-category-theory';
+if (window.location.pathname === '/posts/implicit-products-better-forall.html')
+  window.location.href = '/posts/implicit-products-better-forall';
+if (window.location.pathname === '/posts/logic-in-types.html')
+  window.location.href = '/posts/logic-in-types';
+if (window.location.pathname === '/posts/safe-mmm-with-coeffects.html')
+  window.location.href = '/posts/safe-mmm-with-coeffects';
+if (window.location.pathname === '/posts/security-crashing-modal-logic.html')
+  window.location.href = '/posts/security-crashing-modal-logic';
+if (window.location.pathname === '/posts/simple-programming-languages.html')
+  window.location.href = '/posts/simple-programming-languages';
+if (window.location.pathname === '/posts/type-of-sprintf.html')
+  window.location.href = '/posts/type-of-sprintf';
+if (window.location.pathname === '/posts/typechecking-sabervm.html')
+  window.location.href = '/posts/typechecking-sabervm';
+if (window.location.pathname === '/posts/cricket.html')
+  window.location.href = '/posts/cricket';
+      ",
+      ),
       tail(),
     ]),
   ])
